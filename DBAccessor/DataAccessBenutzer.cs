6ece7d607ce6a2cf413b1ba56573a7263e432373
frosch <frosch@b2958a34-12e8-0310-94c7-a243b39c95bb>
2004-12-10 12:29:01 +0000

    Klassen für den Datenbankzugriff hinzugefügt: Auslesen und Eintragen in die Benutzer-Tabelle funktionieren (Kommentierte Version)

--- DBAccessor/DataAccessBenutzer.cs
+++ DBAccessor/DataAccessBenutzer.cs
@@ -9,16 +9,16 @@ using System.Text;
 namespace DBAccessor
 {
 	/// <summary>
-	/// Zusammenfassung für DataAccessBenutzer.
+	/// Datenzugriffsklasse für die Tabelle benutzer
 	/// </summary>
 	public class DataAccessBenutzer : System.ComponentModel.Component
 	{
-		private System.Data.SqlClient.SqlCommand m_sqlSelectCommand;
-		private System.Data.SqlClient.SqlCommand m_sqlInsertCommand;
-		private System.Data.SqlClient.SqlCommand m_sqlUpdateCommand;
-		private System.Data.SqlClient.SqlCommand m_sqlDeleteCommand;
 		private System.Data.SqlClient.SqlConnection m_sqlConn;
 		private System.Data.SqlClient.SqlDataAdapter m_adpBenutzer;
+		private System.Data.SqlClient.SqlCommand m_cmSelect;
+		private System.Data.SqlClient.SqlCommand m_cmInsert;
+		private System.Data.SqlClient.SqlCommand m_cmUpdate;
+		private System.Data.SqlClient.SqlCommand m_cmDelete;
 		/// <summary>
 		/// Erforderliche Designervariable.
 		/// </summary>
@@ -33,6 +33,9 @@ namespace DBAccessor
 			InitializeComponent();
 		}
 
+		/// <summary>
+		/// Standardkonstruktor
+		/// </summary>
 		public DataAccessBenutzer()
 		{
 			///
@@ -56,6 +59,12 @@ namespace DBAccessor
 			base.Dispose( disposing );
 		}
 
+		/// <summary>
+		/// Stellt ein DataSet aus der Tabelle benutzer zusammen, das den mit selectParams festgelegten
+		/// Suchkriterien entspricht
+		/// </summary>
+		/// <param name="selectParams">Suchkriterien</param>
+		/// <returns>ein DataSet vom Typ DSBenutzer das die gesuchten Datensätze enthält</returns>
 		public DSBenutzer Select(DataParameters selectParams)
 		{
 			StringBuilder sbSelect = new StringBuilder("SELECT * FROM Benutzer");
@@ -79,31 +88,51 @@ namespace DBAccessor
 			return dsResult;
 		}
 
+		/// <summary>
+		/// Stellt ein DataSet zusammen, das alle Datensätze der Tabelle benutzer enthält
+		/// </summary>
+		/// <returns>ein DataSet vom Typ DSBenutzer, das alle Datensätze der Tabelle benutzer enthält</returns>
 		public DSBenutzer Select()
 		{
 			return Select(null);
 		}
 
+		/// <summary>
+		/// Trägt die Änderungen, die an dem DataSet vorgenommen wurden, in die Datenbank ein
+		/// </summary>
+		/// <param name="dsUpdate">ein (verändertes) DataSet vom Typ DSBenutzer</param>
 		public void CommitChanges(DSBenutzer dsUpdate)
 		{
 			m_adpBenutzer.Update(dsUpdate, dsUpdate.benutzer.TableName);
 		}
 
+		/// <summary>
+		/// gibt einen für die Spalte "UserID" der Tabelle benutzer initialisierten SQLParameter zurück
+		/// </summary>
 		public static SqlParameter ParamUserID
 		{
 			get {return new SqlParameter("@UserID", SqlDbType.Int, 4, "UserID");}
 		}
 
+		/// <summary>
+		/// gibt einen für die Spalte "Name" der Tabelle benutzer initialisierten SQLParameter zurück
+		/// </summary>
 		public static SqlParameter ParamName
 		{
 			get {return new SqlParameter("@Name", SqlDbType.VarChar, 50, "Name");}
 		}
 
+		/// <summary>
+		/// gibt einen für die Spalte "Passwort" der Tabelle benutzer initialisierten SQLParameter zurück
+		/// </summary>
 		public static SqlParameter ParamPasswort
 		{
 			get {return new SqlParameter("@Passwort", SqlDbType.VarChar, 50, "Passwort");}
 		}
 
+		/// <summary>
+		/// gibt einen für die Spalte "GruppenID" der Tabelle benutzer initialisierten SQLParameter zurück
+		/// </summary>
 		public static SqlParameter ParamGruppenID
 		{
 			get {return new SqlParameter("@GruppenID", SqlDbType.Int, 4, "GruppenID");}
@@ -117,67 +146,67 @@ namespace DBAccessor
 		private void InitializeComponent()
 		{
 			System.Configuration.AppSettingsReader configurationAppSettings = new System.Configuration.AppSettingsReader();
-			this.m_sqlSelectCommand = new System.Data.SqlClient.SqlCommand();
+			this.m_cmSelect = new System.Data.SqlClient.SqlCommand();
 			this.m_sqlConn = new System.Data.SqlClient.SqlConnection();
-			this.m_sqlInsertCommand = new System.Data.SqlClient.SqlCommand();
-			this.m_sqlUpdateCommand = new System.Data.SqlClient.SqlCommand();
-			this.m_sqlDeleteCommand = new System.Data.SqlClient.SqlCommand();
+			this.m_cmInsert = new System.Data.SqlClient.SqlCommand();
+			this.m_cmUpdate = new System.Data.SqlClient.SqlCommand();
+			this.m_cmDelete = new System.Data.SqlClient.SqlCommand();
 			this.m_adpBenutzer = new System.Data.SqlClient.SqlDataAdapter();
 			// 
-			// m_sqlSelectCommand
+			// m_cmSelect
 			// 
-			this.m_sqlSelectCommand.CommandText = "SELECT UserID, Name, Passwort, GruppenID FROM benutzer";
-			this.m_sqlSelectCommand.Connection = this.m_sqlConn;
+			this.m_cmSelect.CommandText = "SELECT UserID, Name, Passwort, GruppenID FROM benutzer";
+			this.m_cmSelect.Connection = this.m_sqlConn;
 			// 
 			// m_sqlConn
 			// 
 			this.m_sqlConn.ConnectionString = ((string)(configurationAppSettings.GetValue("ConnectionString", typeof(string))));
 			// 
-			// m_sqlInsertCommand
+			// m_cmInsert
 			// 
-			this.m_sqlInsertCommand.CommandText = "INSERT INTO benutzer(Name, Passwort, GruppenID) VALUES (@Name, @Passwort, @Gruppe" +
+			this.m_cmInsert.CommandText = "INSERT INTO benutzer(Name, Passwort, GruppenID) VALUES (@Name, @Passwort, @Gruppe" +
 				"nID); SELECT UserID, Name, Passwort, GruppenID FROM benutzer WHERE (UserID = @@I" +
 				"DENTITY)";
-			this.m_sqlInsertCommand.Connection = this.m_sqlConn;
-			this.m_sqlInsertCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Name", System.Data.SqlDbType.VarChar, 50, "Name"));
-			this.m_sqlInsertCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Passwort", System.Data.SqlDbType.VarChar, 50, "Passwort"));
-			this.m_sqlInsertCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@GruppenID", System.Data.SqlDbType.Int, 4, "GruppenID"));
+			this.m_cmInsert.Connection = this.m_sqlConn;
+			this.m_cmInsert.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Name", System.Data.SqlDbType.VarChar, 50, "Name"));
+			this.m_cmInsert.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Passwort", System.Data.SqlDbType.VarChar, 50, "Passwort"));
+			this.m_cmInsert.Parameters.Add(new System.Data.SqlClient.SqlParameter("@GruppenID", System.Data.SqlDbType.Int, 4, "GruppenID"));
 			// 
-			// m_sqlUpdateCommand
+			// m_cmUpdate
 			// 
-			this.m_sqlUpdateCommand.CommandText = @"UPDATE benutzer SET Name = @Name, Passwort = @Passwort, GruppenID = @GruppenID WHERE (UserID = @Original_UserID) AND (GruppenID = @Original_GruppenID) AND (Name = @Original_Name) AND (Passwort = @Original_Passwort); SELECT UserID, Name, Passwort, GruppenID FROM benutzer WHERE (UserID = @UserID)";
-			this.m_sqlUpdateCommand.Connection = this.m_sqlConn;
-			this.m_sqlUpdateCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Name", System.Data.SqlDbType.VarChar, 50, "Name"));
-			this.m_sqlUpdateCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Passwort", System.Data.SqlDbType.VarChar, 50, "Passwort"));
-			this.m_sqlUpdateCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@GruppenID", System.Data.SqlDbType.Int, 4, "GruppenID"));
-			this.m_sqlUpdateCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_UserID", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "UserID", System.Data.DataRowVersion.Original, null));
-			this.m_sqlUpdateCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_GruppenID", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "GruppenID", System.Data.DataRowVersion.Original, null));
-			this.m_sqlUpdateCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_Name", System.Data.SqlDbType.VarChar, 50, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "Name", System.Data.DataRowVersion.Original, null));
-			this.m_sqlUpdateCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_Passwort", System.Data.SqlDbType.VarChar, 50, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "Passwort", System.Data.DataRowVersion.Original, null));
-			this.m_sqlUpdateCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@UserID", System.Data.SqlDbType.Int, 4, "UserID"));
+			this.m_cmUpdate.CommandText = @"UPDATE benutzer SET Name = @Name, Passwort = @Passwort, GruppenID = @GruppenID WHERE (UserID = @Original_UserID) AND (GruppenID = @Original_GruppenID) AND (Name = @Original_Name) AND (Passwort = @Original_Passwort); SELECT UserID, Name, Passwort, GruppenID FROM benutzer WHERE (UserID = @UserID)";
+			this.m_cmUpdate.Connection = this.m_sqlConn;
+			this.m_cmUpdate.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Name", System.Data.SqlDbType.VarChar, 50, "Name"));
+			this.m_cmUpdate.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Passwort", System.Data.SqlDbType.VarChar, 50, "Passwort"));
+			this.m_cmUpdate.Parameters.Add(new System.Data.SqlClient.SqlParameter("@GruppenID", System.Data.SqlDbType.Int, 4, "GruppenID"));
+			this.m_cmUpdate.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_UserID", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "UserID", System.Data.DataRowVersion.Original, null));
+			this.m_cmUpdate.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_GruppenID", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "GruppenID", System.Data.DataRowVersion.Original, null));
+			this.m_cmUpdate.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_Name", System.Data.SqlDbType.VarChar, 50, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "Name", System.Data.DataRowVersion.Original, null));
+			this.m_cmUpdate.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_Passwort", System.Data.SqlDbType.VarChar, 50, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "Passwort", System.Data.DataRowVersion.Original, null));
+			this.m_cmUpdate.Parameters.Add(new System.Data.SqlClient.SqlParameter("@UserID", System.Data.SqlDbType.Int, 4, "UserID"));
 			// 
-			// m_sqlDeleteCommand
+			// m_cmDelete
 			// 
-			this.m_sqlDeleteCommand.CommandText = "DELETE FROM benutzer WHERE (UserID = @Original_UserID) AND (GruppenID = @Original" +
+			this.m_cmDelete.CommandText = "DELETE FROM benutzer WHERE (UserID = @Original_UserID) AND (GruppenID = @Original" +
 				"_GruppenID) AND (Name = @Original_Name) AND (Passwort = @Original_Passwort)";
-			this.m_sqlDeleteCommand.Connection = this.m_sqlConn;
-			this.m_sqlDeleteCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_UserID", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "UserID", System.Data.DataRowVersion.Original, null));
-			this.m_sqlDeleteCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_GruppenID", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "GruppenID", System.Data.DataRowVersion.Original, null));
-			this.m_sqlDeleteCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_Name", System.Data.SqlDbType.VarChar, 50, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "Name", System.Data.DataRowVersion.Original, null));
-			this.m_sqlDeleteCommand.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_Passwort", System.Data.SqlDbType.VarChar, 50, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "Passwort", System.Data.DataRowVersion.Original, null));
+			this.m_cmDelete.Connection = this.m_sqlConn;
+			this.m_cmDelete.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_UserID", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "UserID", System.Data.DataRowVersion.Original, null));
+			this.m_cmDelete.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_GruppenID", System.Data.SqlDbType.Int, 4, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "GruppenID", System.Data.DataRowVersion.Original, null));
+			this.m_cmDelete.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_Name", System.Data.SqlDbType.VarChar, 50, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "Name", System.Data.DataRowVersion.Original, null));
+			this.m_cmDelete.Parameters.Add(new System.Data.SqlClient.SqlParameter("@Original_Passwort", System.Data.SqlDbType.VarChar, 50, System.Data.ParameterDirection.Input, false, ((System.Byte)(0)), ((System.Byte)(0)), "Passwort", System.Data.DataRowVersion.Original, null));
 			// 
 			// m_adpBenutzer
 			// 
-			this.m_adpBenutzer.DeleteCommand = this.m_sqlDeleteCommand;
-			this.m_adpBenutzer.InsertCommand = this.m_sqlInsertCommand;
-			this.m_adpBenutzer.SelectCommand = this.m_sqlSelectCommand;
+			this.m_adpBenutzer.DeleteCommand = this.m_cmDelete;
+			this.m_adpBenutzer.InsertCommand = this.m_cmInsert;
+			this.m_adpBenutzer.SelectCommand = this.m_cmSelect;
 			this.m_adpBenutzer.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
 																									new System.Data.Common.DataTableMapping("Table", "benutzer", new System.Data.Common.DataColumnMapping[] {
 																																																				new System.Data.Common.DataColumnMapping("UserID", "UserID"),
 																																																				new System.Data.Common.DataColumnMapping("Name", "Name"),
 																																																				new System.Data.Common.DataColumnMapping("Passwort", "Passwort"),
 																																																				new System.Data.Common.DataColumnMapping("GruppenID", "GruppenID")})});
-			this.m_adpBenutzer.UpdateCommand = this.m_sqlUpdateCommand;
+			this.m_adpBenutzer.UpdateCommand = this.m_cmUpdate;
 
 		}
 		#endregion
