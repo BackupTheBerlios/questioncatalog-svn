69663e4d72c20f3ac59c1491a3b74c7e14aed4a2
erni01 <erni01@b2958a34-12e8-0310-94c7-a243b39c95bb>
2005-01-26 15:14:36 +0000

    registrieren: nav und menü eingefügt

--- UmfrageEditorWebFiles/registrieren.aspx
+++ UmfrageEditorWebFiles/registrieren.aspx
@@ -22,20 +22,88 @@
 						<table cellSpacing="0" cellPadding="0" width="100%" border="0">
 							<tr>
 								<td vAlign="top" align="left" width="120">
+									<TABLE id="Table1" cellSpacing="0" cellPadding="5" width="100%" border="0">
+										<TR>
+											<TD>
+												<DIV id="m_menu_default" style="WIDTH: 120px" runat="server" ms_positioning="FlowLayout">
+													<P class="pagename" align="center">Navigation</P>
+													<A class="menu" href="default.aspx" target="_parent">Startseite </A>
+												</DIV>
+												<DIV id="m_menu_registrieren" style="WIDTH: 120px" runat="server" ms_positioning="FlowLayout"><A class="menu" href="registrieren.aspx" target="_parent">Registrieren</A>
+												</DIV>
+												<DIV id="m_menu_admin" style="WIDTH: 120px" runat="server" ms_positioning="FlowLayout"><A class="menu" href="administration.aspx" target="_parent">Administration</A>
+												</DIV>
+												<DIV class="menu" id="m_menu_user" style="WIDTH: 120px" runat="server" ms_positioning="FlowLayout"><A class="menu" href="defaultuser.aspx" target="_parent">User 
+														Profil</A>
+												</DIV>
+												<BR>
+												<DIV id="m_menu_debug" style="WIDTH: 120px" runat="server" ms_positioning="FlowLayout">
+													<P class="pagename" align="center">Debug Navigation</P>
+													<P><A class="menu" href="administration.aspx" target="_parent">administration</A><BR>
+														<A class="menu" href="antworten.aspx" target="_parent">antworten</A><BR>
+														<A class="menu" href="default.aspx" target="_parent">default</A><BR>
+														<A class="menu" href="defaultuser.aspx" target="_parent">defaultuser</A><BR>
+														<A class="menu" href="ergebnisse.aspx.cs" target="_parent">ergebnisse</A><BR>
+														<A class="menu" href="fragedarstellung.aspx" target="_parent">fragedarstellung</A><BR>
+														<A class="menu" href="login.aspx" target="_parent">login</A><BR>
+														<A class="menu" href="loginstatus.aspx" target="_parent">loginstatus</A><BR>
+														<A class="menu" href="registrieren.aspx" target="_parent">registrieren</A><BR>
+														<A class="menu" href="umfrageergebnisse.aspx" target="_parent">umfrageergebnisse</A><BR>
+														<A class="menu" href="umfrageerstellen.aspx" target="_parent">umfrageerstellen</A>
+													</P>
+												</DIV>
+											</TD>
+										</TR>
+									</TABLE>
 								</td>
 								<TD vAlign="top" align="left">
 									<P></P>
-									&nbsp;
 									<P></P>
 									<P>
-										<asp:Label id="Label1" runat="server">Benutzername</asp:Label><BR>
-										<asp:TextBox id="txtBenutzername" runat="server"></asp:TextBox></P>
-									<P>
-										<asp:Label id="Label2" runat="server">Passwort</asp:Label><BR>
-										<asp:TextBox id="txtPasswort" runat="server"></asp:TextBox></P>
-									<P><INPUT id="btnRegistrieren" type="submit" value="Registrieren" name="Submit1" runat="server"></P>
-									<DIV id="lbAusgabe" style="DISPLAY: inline; WIDTH: 70px; HEIGHT: 15px" runat="server"
-										ms_positioning="FlowLayout">Ausgabe</DIV>
+										<TABLE id="Table2" cellSpacing="0" cellPadding="0" width="100%" border="0">
+											<TR>
+												<TD style="HEIGHT: 18px">
+													<DIV align="right">
+														<DIV id="m_login" style="HEIGHT: 18px" align="right" runat="server" ms_positioning="FlowLayout">
+															<asp:Label id="lbLoginMessage" runat="server" CssClass="login" ForeColor="Red"></asp:Label>&nbsp;&nbsp;
+															<SPAN class="login">Benutzername</SPAN>
+															<asp:textbox id="Textbox2" runat="server" Width="60px" BorderWidth="1px" BorderStyle="Solid"
+																Height="16px" Font-Size="10px" MaxLength="20"></asp:textbox>&nbsp;&nbsp; <SPAN class="login">
+																Passwort</SPAN>
+															<asp:textbox id="Textbox1" runat="server" Width="60px" BorderWidth="1px" BorderStyle="Solid"
+																Height="16px" Font-Size="10px" MaxLength="20" TextMode="Password"></asp:textbox>&nbsp;&nbsp;
+															<asp:LinkButton id="LinkLogin" runat="server" CssClass="function">Login</asp:LinkButton>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+														</DIV>
+														<DIV align="right">
+															<DIV id="m_logout" style="HEIGHT: 18px" runat="server" ms_positioning="FlowLayout">
+																<asp:Label id="lbLoginStatus" runat="server" CssClass="login"></asp:Label>
+																<asp:LinkButton id="LinkLogout" runat="server" CssClass="function">Logout</asp:LinkButton>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+															</DIV>
+														</DIV>
+													</DIV>
+												</TD>
+											</TR>
+											<TR>
+												<TD>
+													<P><BR>
+														Hier können Sie sich registrieren um selbst Umfragen erstellen zu können.</P>
+													<P>Wichtig!!!<BR>
+														Um an Unfragen teilnehmen zu können ist es nicht erforderlich sich zu 
+														registrieren.<BR>
+														Wählen Sie dazu auf der Startseite eine beliebige Umfrage aus.</P>
+													<P>
+														<asp:Label id="Label1" runat="server">Benutzername</asp:Label><BR>
+														<asp:TextBox id="txtBenutzername" runat="server"></asp:TextBox></P>
+													<P>
+														<asp:Label id="Label2" runat="server">Passwort</asp:Label><BR>
+														<asp:TextBox id="txtPasswort" runat="server"></asp:TextBox></P>
+													<P><INPUT id="btnRegistrieren" type="submit" value="Registrieren" name="Submit1" runat="server"></P>
+													<DIV id="lbAusgabe" style="DISPLAY: inline; WIDTH: 496px; COLOR: red; HEIGHT: 15px" runat="server"
+														ms_positioning="FlowLayout"></DIV>
+												</TD>
+											</TR>
+										</TABLE>
+									</P>
 								</TD>
 							</tr>
 						</table>
--- UmfrageEditorWebFiles/registrieren.aspx.cs
+++ UmfrageEditorWebFiles/registrieren.aspx.cs
@@ -11,6 +11,9 @@ using System.Web.UI.HtmlControls;
 using DBAccessor;
 using System.Data.SqlClient;
 
+using System.Configuration;
+
+
 namespace UmfrageEditor
 {
 	/// <summary>
@@ -24,12 +27,26 @@ namespace UmfrageEditor
 		protected System.Web.UI.HtmlControls.HtmlInputButton btnRegistrieren;
 		protected System.Web.UI.WebControls.Label Label2;
 		protected System.Web.UI.HtmlControls.HtmlGenericControl lbAusgabe;
+		protected System.Web.UI.HtmlControls.HtmlGenericControl m_menu_default;
+		protected System.Web.UI.HtmlControls.HtmlGenericControl m_menu_registrieren;
+		protected System.Web.UI.HtmlControls.HtmlGenericControl m_menu_admin;
+		protected System.Web.UI.HtmlControls.HtmlGenericControl m_menu_user;
+		protected System.Web.UI.HtmlControls.HtmlGenericControl m_menu_debug;
+		protected System.Web.UI.WebControls.LinkButton LinkLogout;
+		protected System.Web.UI.WebControls.Label lbLoginStatus;
+		protected System.Web.UI.WebControls.LinkButton LinkLogin;
+		protected System.Web.UI.WebControls.TextBox Textbox1;
+		protected System.Web.UI.WebControls.TextBox Textbox2;
+		protected System.Web.UI.WebControls.Label lbLoginMessage;
+		protected System.Web.UI.HtmlControls.HtmlGenericControl m_login;
+		protected System.Web.UI.HtmlControls.HtmlGenericControl m_logout;
 
-		protected DataAccessBenutzer daBenutzer;
+		protected DataAccessBenutzer daBenutzer = new DataAccessBenutzer();
 	
 		private void Page_Load(object sender, System.EventArgs e)
 		{
-			daBenutzer = new DataAccessBenutzer();
+			// Einblendungen für Login und Navmenü prüfen
+			check_visibility();
 		}
 
 		#region Vom Web Form-Designer generierter Code
@@ -48,6 +65,8 @@ namespace UmfrageEditor
 		/// </summary>
 		private void InitializeComponent()
 		{    
+			this.LinkLogin.Click += new System.EventHandler(this.LinkLogin_Click);
+			this.LinkLogout.Click += new System.EventHandler(this.LinkLogout_Click);
 			this.txtBenutzername.TextChanged += new System.EventHandler(this.txtBenutzername_TextChanged);
 			this.txtPasswort.TextChanged += new System.EventHandler(this.txtPasswort_TextChanged);
 			this.btnRegistrieren.ServerClick += new System.EventHandler(this.btnRegistrieren_ServerClick);
@@ -89,46 +108,6 @@ namespace UmfrageEditor
 			}
 		}
 
-//		private void btnLogin_Click(object sender, System.EventArgs e)
-//		{
-//			SqlParameter paramName = DataAccessBenutzer.ParamName;
-//			paramName.Value = txtBenutzername.Text;
-//			DataParameters dParams = new DataParameters();
-//			dParams.Add(paramName);
-//			DSBenutzer dsBen = daBenutzer.Select(dParams);
-//
-//			if(dsBen.benutzer.Rows.Count == 1)
-//			{
-//				string pw = (string)dsBen.benutzer.Rows[0]["Passwort"];
-//				if (pw.Equals( txtPasswort.Text))
-//				{
-//					string username = (string)dsBen.benutzer.Rows[0]["Name"];
-//					SessionContainer.ReadFromSession(this).User.Login(username, pw);
-////					lbLoginStatus.Text = "Login erfolgreich!";
-//				}
-//				else
-//				{
-//					SessionContainer.ReadFromSession(this).User.Logout();
-////					lbLoginStatus.Text = "Falsches Passwort!";
-//				}
-//			}
-//			else
-//			{
-//				SessionContainer.ReadFromSession(this).User.Logout();
-////				lbLoginStatus.Text = "Falscher Benutzername!";
-//			}
-//
-//			if (SessionContainer.ReadFromSession(this).User.IsLoggedIn)
-//			{
-////				lbLoginStatus.Text = "Login erfolgreich!";
-//				Server.Transfer("defaultuser.aspx");
-//			}
-//			else
-//			{
-//				lbLoginStatus.Text = "Login fehlgeschlagen!";
-//			}
-//		}
-//
 		private void txtBenutzername_TextChanged(object sender, System.EventArgs e)
 		{
 		
@@ -138,5 +117,180 @@ namespace UmfrageEditor
 		{
 		
 		}
+
+		#region Login & Menü
+		private void LinkLogin_Click(object sender, System.EventArgs e)
+		{
+			SqlParameter paramName = DataAccessBenutzer.ParamName;
+			paramName.Value = txtBenutzername.Text;
+			DataParameters dParams = new DataParameters();
+			dParams.Add(paramName);
+			DSBenutzer dsBen = daBenutzer.Select(dParams);
+
+			if(dsBen.benutzer.Rows.Count == 1)
+			{
+				string pw = (string)dsBen.benutzer.Rows[0]["Passwort"];
+				if (pw.Equals( txtPasswort.Text))
+				{
+					// Login erfolgreich
+					string username = (string)dsBen.benutzer.Rows[0]["Name"];
+					SessionContainer.ReadFromSession(this).User.Login(username, pw);
+					check_visibility();
+
+					// Statusmessages setzen
+					lbLoginStatus.Text = @"Eingeloggt als """ + SessionContainer.ReadFromSession(this).User.Username + @"""";
+					lbLoginMessage.Text = "";
+
+					// Redirect zur persönlichen Startseite
+					Server.Transfer("defaultuser.aspx");
+
+				}
+				else
+				{
+					// Falsches Passwort
+					
+					// Überprüfung auf Debugmodus
+					if(!DBConstants.Debugmodus)
+					{
+						// Standardausgabe
+						lbLoginMessage.Text = "Login fehlgeschlagen!";
+					}
+					else
+					{
+						// Ausgabe im Debugmodus
+						lbLoginMessage.Text = "Falsches Passwort!";
+					}
+
+					// Zur Sicherheit abmelden
+					SessionContainer.ReadFromSession(this).User.Logout();
+					
+					// Statusmessages setzen
+					lbLoginStatus.Text = "";
+
+					// Sichtbarkeiten neu festlegen
+					check_visibility();
+
+				}
+			}
+			else
+			{
+				// Falscher Benutzer
+
+				// Überprüfung auf Debugmodus
+				if(!DBConstants.Debugmodus)
+				{
+					// Standardausgabe
+					lbLoginMessage.Text = "Login fehlgeschlagen!";
+				}
+				else
+				{
+					// Ausgabe im Debugmodus
+					lbLoginMessage.Text = "Unbekannter Benutzer!";
+				}
+
+				// Zur Sicherheit abmelden
+				SessionContainer.ReadFromSession(this).User.Logout();
+
+				// Statusmessages setzen
+				lbLoginStatus.Text = "";
+
+				// Sichtbarkeiten neu festlegen
+				check_visibility();
+			}
+		}
+
+		private void LinkLogout_Click(object sender, System.EventArgs e)
+		{
+			m_login.Visible = true;
+			m_logout.Visible = false;
+			SessionContainer.ReadFromSession(this).User.Logout();
+			Server.Transfer("default.aspx");
+		}
+
+		private void check_visibility()
+		{
+			/* Sichtbarkeiten festlegen */
+			
+			#region Loginblock
+			if (!SessionContainer.ReadFromSession(this).User.IsLoggedIn)
+			{
+				//wenn User nicht eingeloggt ist
+
+				// Zeige Login 
+				m_login.Visible = true;
+
+				// verstecke Logout
+				m_logout.Visible = false;
+
+				// Messages entsprechend setzen
+				lbLoginStatus.Text = "";
+
+			}
+			else
+			{
+				// wenn User eingeloggt ist
+
+				// Verstecke Login
+				m_login.Visible = false;
+
+				// Zeige Logout 
+				m_logout.Visible = true;
+
+				// Messages entsprechend setzen
+				lbLoginMessage.Text = "";
+				lbLoginStatus.Text = @"Eingeloggt als """ + SessionContainer.ReadFromSession(this).User.Username + @"""";
+			}
+			#endregion
+
+			#region Navigationsmenü 
+			
+			// Alle Menüs bis auf Widerruf deaktivieren
+			m_menu_default.Visible = false;
+			m_menu_registrieren.Visible = false;
+			m_menu_user.Visible = false;
+			m_menu_admin.Visible = false;
+			m_menu_debug.Visible = false;
+
+			
+			// Einblenden des generellen Navigationsblocks
+			m_menu_default.Visible = true;
+
+			// Einblendung im Navigationmenü prüfen
+			/* Im Debugmodus Direktnavigation zu den Seiten einblenden 
+				 * und alle Menüs einblenden */
+			if (DBConstants.Debugmodus)
+			{
+				m_menu_default.Visible = true;
+				m_menu_registrieren.Visible = true;
+				m_menu_user.Visible = true;
+				m_menu_admin.Visible = true;
+				m_menu_debug.Visible = true;
+			}
+				// wenn der Benutzer angemeldet ist 
+			else if (SessionContainer.ReadFromSession(this).User.IsLoggedIn)
+			{
+				// Usermenü anzeigen 
+				m_menu_user.Visible = true;
+				
+				// wenn der Benutzer AdminStatus besitzt
+				if (SessionContainer.ReadFromSession(this).User.IsAdmin)
+				{
+					// Adminmenü anzeigen
+					m_menu_admin.Visible = true;
+				}
+
+			}
+				// Wenn der Benutzer nicht angemeldet ist
+			else
+			{
+				// Registrieren anzeigen
+				m_menu_registrieren.Visible = true;
+			}
+			#endregion
+
+
+		}
+
+		#endregion
 	}
 }
